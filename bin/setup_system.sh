2a4600fd14c76a9e8a07260e2cb85cdfbe213e0e
Michael Berghammer <info@mbelab.de>
2026-06-12 17:21:51 +0200

    implement setup_system script

--- bin/setup_system.sh
+++ bin/setup_system.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-# Setup system script
+# Setup system script (sudo)
+# mbelab
+
+
+# script
+sudo -v
+
+option="$1"
+
+# update package index
+sudo apt-get update
+
+# install base packages
+sudo apt-get install -y gcc g++ gdb make cmake pkgconf
+
+# install OpenBLAS packages
+sudo apt-get install -y libopenblas-dev
+
+# install Vulkan packages depending on option
+if [[ $option = '--vulkan' ]]
+then
+    sudo apt-get install -y libvulkan-dev glslc spirv-headers
+fi
