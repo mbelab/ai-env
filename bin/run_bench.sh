32e28c5f6287b2a461aeed5b2dd4f667c12aa6ed
Michael Berghammer <info@mbelab.de>
2026-06-12 18:57:19 +0200

    add and implement run_bench script

---
+++ bin/run_bench.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Run model in benchmark script
+# mbelab
+
+
+# defines
+MODEL_LIST='./data/model_list.json'
+LLAMA_BIN_DIR='./llama.cpp/build/bin'
+
+
+# script
+model_list=$(realpath $MODEL_LIST)
+model_path=$(dirname $model_list)/'models'
+
+model="$1"
+
+# handle model
+model_type=$(jq -r --arg model $model '.models[$model].type' $model_list)
+model_file=$model_path/$model'.'$model_type
+
+# run model in benchmark
+$LLAMA_BIN_DIR/llama-bench -m $model_file
