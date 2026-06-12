79c3e6905b418f101ffaac45d22eaf69b9274eba
Michael Berghammer <info@mbelab.de>
2026-06-12 17:37:41 +0200

    implement setup_llama_cpp script

--- bin/setup_llama_cpp.sh
+++ bin/setup_llama_cpp.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
 # Setup llama.cpp script
+# mbelab
+
+
+# defines
+LLAMA_DIR='./llama.cpp'
+
+
+# script
+option="$1"
+
+# enter llama.cpp directory
+cd $LLAMA_DIR
+
+# prepare cmake build depending on option
+if [[ $option = '--vulkan' ]]
+then
+    cmake -B build -DGGML_VULKAN=ON
+else
+    cmake -B build -DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS
+fi
+
+# build llama.cpp
+cmake --build build --config Release -j
