82cb02680d28f8f58129b08aff7d578a81d0a3a1
Michael Berghammer <info@mbelab.de>
2026-07-23 22:08:54 +0200

    support router mode in run_server script

--- bin/run_server.sh
+++ bin/run_server.sh
@@ -15,43 +15,54 @@ model_list=$AI_ENV_MODEL_LIST
 model_path=$AI_ENV_MODEL_PATH
 
 model="$1"
-shift
+
+# prepare run options
+run_options=(
+    --host $DEFAULT_HOST
+    --port $DEFAULT_PORT
+    --ui
+    --jinja
+    --tools all
+)
 
 # handle model
 if [[ -z $model ]]
 then
-    echo 'No model provided. Abort.'
-    exit 1  # operation not permitted
-fi
+    # start server in router mode
+    run_options+=(
+        --models-dir $model_path
+        --no-models-autoload
+    )
 
-model_file=$(jq -r --arg model $model '.models[$model].model_file' $model_list)
-mmproj_file=$(jq -r --arg model $model '.models[$model].mmproj_file' $model_list)
+    echo 'Run server in router mode...'
+else
+    # start server with defined model
+    shift
 
-if [[ ! -e $model_path/$model/$model_file ]]
-then
-    echo 'Model file not found. Abort.'
-    exit 2  # no such file or directory
-fi
+    model_file=$(jq -r --arg model $model '.models[$model].model_file' $model_list)
+    mmproj_file=$(jq -r --arg model $model '.models[$model].mmproj_file' $model_list)
 
-# prepare run options
-run_options=(
-    --model $model_path/$model/$model_file
-    --alias $DEFAULT_ALIAS
-    --ui
-    --jinja
-    --tools all
-    --host $DEFAULT_HOST
-    --port $DEFAULT_PORT
-)
+    if [[ ! -e $model_path/$model/$model_file ]]
+    then
+        echo 'Model file not found. Abort.'
+        exit 2  # no such file or directory
+    fi
 
-if [[ ! -z $mmproj_file ]]
-then
-    run_options+=(--mmproj $model_path/$model/$mmproj_file)
+    run_options+=(
+        --model $model_path/$model/$model_file
+        --alias $DEFAULT_ALIAS
+    )
+
+    if [[ ! -z $mmproj_file ]]
+    then
+        run_options+=(--mmproj $model_path/$model/$mmproj_file)
+    fi
+
+    echo 'Run server with model '$model' as '$DEFAULT_ALIAS'...'
 fi
 
 # run model as server
-echo 'Run model '$model' as server with alias '$DEFAULT_ALIAS
-echo '(http://'$DEFAULT_HOST':'$DEFAULT_PORT')...'
+echo '(http://'$DEFAULT_HOST':'$DEFAULT_PORT')'
 echo
 echo 'Press CTRL+C to stop server.'
 echo
